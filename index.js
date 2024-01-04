e4fb1593dbcb970c2318cdecbfab90f15b838f81
abdullahal5 <abdullahalfahin183@gmail.com>
2024-01-04 14:03:50 -0800

    working...

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const Port = 5000;
 const cors = require("cors");
 const dotenv = require("dotenv");
 require("dotenv").config();
-const jwt = require("jsonwebtoken")
+const jwt = require("jsonwebtoken");
 
 app.use(cors());
 app.use(express.json());
@@ -35,99 +35,123 @@ async function run() {
 
     // jwt authentication
 
-    app.post("/jwt", async(req, res) =>{
+    app.post("/jwt", async (req, res) => {
       const user = req.body;
       const token = jwt.sign(user, process.env.JWT_TOKEN, {
         expiresIn: "1hr",
       });
-      res.send({token})
-    })
+      res.send({ token });
+    });
 
-    const verifyToken = (req, res, next) =>{
+    const verifyToken = (req, res, next) => {
       if (!req.headers.authorization) {
-        return res.status(401).send({message: "unauthorized access"})
+        return res.status(401).send({ message: "unauthorized access" });
       }
       const token = req.headers.authorization.split(" ")[1];
-      jwt.verify(token, process.env.JWT_TOKEN, (err,decoded) =>{
-        if(err){
-          return res.status(401).send({message: "token is invalid!"})
+      jwt.verify(token, process.env.JWT_TOKEN, (err, decoded) => {
+        if (err) {
+          return res.status(401).send({ message: "token is invalid!" });
         }
-        req.decoded = decoded
-        next()
-      })
-    }
+        req.decoded = decoded;
+        next();
+      });
+    };
 
-    const verifyAdmin = async (req, res, next) =>{
+    const verifyAdmin = async (req, res, next) => {
       const email = req?.decoded?.email;
-      const query = {email: email};
+      const query = { email: email };
 
-      const user = await usersCollection.findOne(query)
+      const user = await usersCollection.findOne(query);
       const isAdmin = user?.role === "Admin";
 
-      if(!isAdmin){
-        return res.status(403).send({message: "forbidden access!"})
+      if (!isAdmin) {
+        return res.status(403).send({ message: "forbidden access!" });
       }
-      next()
-    }
+      next();
+    };
 
     app.get("/menu", async (req, res) => {
       const result = await menuCollection.find().toArray();
       res.send(result);
     });
 
-    app.get("/menu/:id", async(req, res) =>{
+    app.get("/menu/:id", async (req, res) => {
       const id = req.params.id;
-      const filter = {_id: new ObjectId(id)}
-      const result = await menuCollection.findOne(filter)
-      res.send(result)
-    })
+      const filter = { _id: new ObjectId(id) };
+      const result = await menuCollection.findOne(filter);
+      res.send(result);
+    });
+
+    app.post("/cartItems", verifyToken, verifyAdmin, async (req, res) => {
+      const body = req.body;
+      const result = await cartCollection.insertOne(body);
+      res.send(result);
+    });
 
     app.get("/cartItems", async (req, res) => {
       const result = await cartCollection.find().toArray();
       res.send(result);
     });
 
-    app.get("/cartItems/:id", async (req, res) => {
+    app.get("/cartItems/:id",  async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await cartCollection.findOne(query);
       res.send(result);
     });
 
-    app.delete("/cartItems/:id", async (req, res) => {
+    app.delete("/cartItems/:id", verifyToken, verifyAdmin, async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await cartCollection.deleteOne(query);
       res.send(result);
     });
 
-    app.post("/carts", async (req, res) => {
+    app.patch("/cartItems/:id", verifyToken, verifyAdmin, async(req, res) =>{
+      const id = req.params.id;
+      const {name, recipe, category, image, price} = req.body;
+      const filter = { _id: new ObjectId(id) };
+      const option = { upsert: true };
+      const updatedDoc = {
+        $set: {
+          name: name,
+          recipe: recipe,
+          category: category,
+          image: image,
+          price: price
+        }
+      }
+      const result = await cartCollection.updateOne(filter, updatedDoc, option)
+      res.send(result)
+    })
+
+    app.post("/add", async (req, res) => {
       const cartItem = req.body;
       const result = await addCollection.insertOne(cartItem);
       res.send(result);
     });
-    app.get("/carts", verifyToken, async (req, res) => {
+    app.get("/add", verifyToken, async (req, res) => {
       const email = req.query.email;
       const filter = { email: email };
       const result = await addCollection.find(filter).toArray();
       res.send(result);
     });
 
-    app.get("/add/:id", async (req, res) => {
+    app.get("/add/:id", verifyToken, async (req, res) => {
       const id = req.params.id;
       const filter = { _id: new ObjectId(id) };
       const result = await addCollection.findOne(filter);
       res.send(result);
     });
 
-    app.delete("/add/:id", async (req, res) => {
+    app.delete("/add/:id", verifyToken, async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await addCollection.deleteOne(query);
       res.send(result);
     });
 
-    app.put("/add/:id", async (req, res) => {
+    app.put("/add/:id", verifyToken, async (req, res) => {
       const id = req.params.id;
       const { quantity } = req.body;
       const query = { _id: new ObjectId(id) };
@@ -143,7 +167,7 @@ async function run() {
 
     app.get("/users/admin/:email", verifyToken, async (req, res) => {
       const email = req?.params?.email;
-      
+
       if (email !== req?.decoded?.email) {
         return res.status(403).send({ message: "forbidden access" });
       }
@@ -169,7 +193,7 @@ async function run() {
       }
     });
 
-    app.get("/users", verifyToken, verifyAdmin, async (req, res) => {
+    app.get("/users", verifyToken,  async (req, res) => {
       const result = await usersCollection.find().toArray();
       res.send(result);
     });
